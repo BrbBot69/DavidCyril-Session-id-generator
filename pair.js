19a545225a2899804309f33c7c253c6835f83b3d
BrbBot69 <148257126+BrbBot69@users.noreply.github.com>
2025-08-09 14:30:26 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by ZENITSU*
+*_Pair Code Connected by Ballas 𝗑ᴾᴿᴼ*
 *_Made With 🚬🗿_*
 ______________________________________
 ╔════◇
-║ *『 AMAZING YOU'VE CHOSEN ZENITSU CRASH V2 』*
+║ *『 AMAZING YOU'VE CHOSEN Ballas 𝗑ᴾᴿᴼ 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
 ║❒ *Ytube:* _https://www.youtube.com/@BTSMODZ
-║❒ *Owner:* https://wa.me/2348075952205_
-║❒ *Repo:* _https://github.com/Fearless-tech1_
-║❒ *WaGroup:* _https://chat.whatsapp.com/C3GFThC0tIpGaJY9DFUeCK
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VahusSh0QeaoFzHJCk2x
-║❒ *Plugins:* _https://github.com/Fearless-tech1 
+║❒ *Owner:* _https://wa.me/27767494368_
+║❒ *Repo:* _https://github.com/BrbBot69_
+║❒ *WaGroup:* _https://chat.whatsapp.com/DxZQNaucyDiDCeQmcjBxbV?mode=ac_t_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbA9WaqEQIakf3fQFu1t_
+║❒ *Plugins:* _https://github.com/Fearless-tech1_ 
 ╚════════════════════════╝
 _____________________________________
 
